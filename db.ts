3bf1453323c66ae03092e9905e7e6723ebc86f7c
Harold <chipuriroh@gmail.com>
2025-03-31 08:07:01 +0200

    Updated db.ts to use environment variables and secured credentials

--- db.ts
+++ db.ts
@@ -4,6 +4,6 @@ export const db = await new Client().connect({
   hostname: Deno.env.get("MYSQLHOST") || "127.0.0.1",
   username: Deno.env.get("MYSQLUSER") || "root",
   db: Deno.env.get("MYSQLDATABASE") || "votesbackend",
-  password: Deno.env.get("MYSQLPASSWORD") || "2812",
+  password: Deno.env.get("MYSQLPASSWORD") || "",
   port: Number(Deno.env.get("MYSQLPORT")) || 3306,
 });
